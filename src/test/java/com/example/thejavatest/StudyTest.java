eadd306e852910c647ccce664f04eb1fc97d8b8e
suuuuzy <jangsooji91@gmail.com>
2023-02-13 11:46:27 +0900

    Assertion

--- src/test/java/com/example/thejavatest/StudyTest.java
+++ src/test/java/com/example/thejavatest/StudyTest.java
@@ -2,6 +2,8 @@ package com.example.thejavatest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -10,9 +12,21 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDe31")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+
+        Study study = new Study(10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + "상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
+
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+
     }
 
     @Test
