1a68a20c8291e7fa7e9c8f73181a2f3d47a8221c
suuuuzy <jangsooji91@gmail.com>
2023-02-14 20:59:20 +0900

    Mock 객체 Stubbing

--- src/test/java/com/example/thejavatest/study/StudyServiceTest.java
+++ src/test/java/com/example/thejavatest/study/StudyServiceTest.java
@@ -1,12 +1,18 @@
 package com.example.thejavatest.study;
 
+import com.example.thejavatest.domain.Member;
 import com.example.thejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -17,6 +23,45 @@ class StudyServiceTest {
 
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+
+        // Mock 객체를 조작해서
+        // 특정한 매개변수를 받은 경우, 특정한 값을 리턴하거나 예외를 던지도록 만들 수 있다.
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("suz1wkd@gmail.com");
+
+//        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        when(memberService.findById(any())).thenReturn(Optional.of(member)); //ArgumentMatchers
+
+        assertEquals("suz1wkd@gmail.com", memberService.findById(1L).get().getEmail());
+        assertEquals("suz1wkd@gmail.com", memberService.findById(2L).get().getEmail());
+
+
+        // Mock 객체의 행동: void 메소드는 예외를 던지지 않고 아무런 일도 발생하지 않는다.
+        // Mock 객체를 조작해서, void 메소드가 특정 매개변수를 받거나 호출된 경우 예외를 발생시킬 수 있다.
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
+        memberService.validate(2L);
+
+
+        // Mock 객체를 조작해서
+        // 메소드가 동일한 매개변수로 여러번 호출될 때, 각기 다르게 행동하도록 조작할 수 있다.
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member)) //첫 번째 호출
+                .thenThrow(new RuntimeException()) //두 번째 호출
+                .thenReturn(Optional.empty()); //세 번째 호출
+
+        Optional<Member> findMember = memberService.findById(1L);
+        assertEquals("suz1wkd@gmail.com", findMember.get().getEmail());
+
+        assertThrows(RuntimeException.class, () -> memberService.findById(2L));
+
+        assertEquals(Optional.empty(), memberService.findById(3L));
     }
 
 }
